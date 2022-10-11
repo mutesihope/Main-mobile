b81c6add0406dd206e6c5cc3b2cce8a793174207
mutesihope <hopemutesi250@gmail.com>
2022-10-11 17:29:19 +0200

    Fix es lint errors

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
+/* eslint-disable linebreak-style */
 const hamburger = document.getElementById('mobile-nav');
- const dropDown = document.getElementById('desktop-nav');
- const icon = document.querySelector('.fa-bars');
- const links = document.querySelectorAll('.nav-link');
- const showNo = document.querySelector('.mobile-none');
+const dropDown = document.getElementById('desktop-nav');
+const icon = document.querySelector('.fa-bars');
+const links = document.querySelectorAll('.nav-link');
 
 let isShown = false;
-hamburger.addEventListener("click", function(){ 
-   isShown = !isShown
-   if(isShown === true){
-     dropDown.style.display = "flex" 
-     hamburger.style.color = 'black'
-     icon.className = "fa-regular fa-x"
-     icon.style.fontSize = '15px'
-     dropDown.classList.remove('active')
-   }
-   else{
-     dropDown.style.display = "none"
-
-    icon.className = "fa-solid fa-bars"
-
-   }
-})
+hamburger.addEventListener('click', () => {
+  isShown = !isShown;
+  if (isShown === true) {
+    dropDown.style.display = 'flex';
+    hamburger.style.color = 'black';
+    icon.className = 'fa-regular fa-x';
+    icon.style.fontSize = '15px';
+    dropDown.classList.remove('active');
+  } else {
+    dropDown.style.display = 'none';
+    icon.className = 'fa-solid fa-bars';
+  }
+});
 
 links.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        // dropDown.style.display = "none" 
-        dropDown.classList.toggle('active')
-        icon.className = 'fa-solid fa-bars'
-        
-    });
+  btn.addEventListener('click', () => {
+    dropDown.classList.toggle('active');
+    icon.className = 'fa-solid fa-bars';
   });
+});
